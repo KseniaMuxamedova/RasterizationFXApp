5381f0ef2ac9111f7f94e6c5e665e53eb1d72794
KseniaMyh <vy33frb8>
2022-10-28 20:01:38 +0300

    version 2, наводим красоту

--- src/main/java/com/cgvsu/rasterization/Rasterization.java
+++ src/main/java/com/cgvsu/rasterization/Rasterization.java
@@ -10,33 +10,27 @@ public class Rasterization {
             final GraphicsContext graphicsContext,
             final int x0, final int y0,
             final int a, final int b,
-            final Color color){
+            final Color startColor,
+            final Color tailColor ){
         final PixelWriter pixelWriter = graphicsContext.getPixelWriter();
 
-        int x1, x2;
+        int x1, x2, x;
         for(int i = y0 - b; i <= y0 + b; i++){
-            x1 = x0 - (int) Math.sqrt(a * a * (1 - (float)(i - y0) * (float)(i - y0) / (float)b /(float)b));
-            x2 = x0 + (int) Math.sqrt(a * a * (1 - (float)(i - y0) * (float)(i - y0) / (float)b /(float)b));
-
-//            System.out.println( (1 - (float)(i - y0) * (i - y0) / b /b));
-
+            x = (int) Math.sqrt(a * a * (1 - (float)(i - y0) * (float)(i - y0) / (float)b /(float)b));
+            x1 = x0 - x;
+            x2 = x0 + x;
             for (int j = x1; j <= x2; j++){
-                pixelWriter.setColor(j, i , color);
+                pixelWriter.setColor(j, i , startColor);
             }
         }
 
     }
-
-    public static void drawRectangle(
-            final GraphicsContext graphicsContext,
-            final int x, final int y,
-            final int width, final int height,
-            final Color color)
-    {
-        final PixelWriter pixelWriter = graphicsContext.getPixelWriter();
-
-        for (int row = y; row < y + height; ++row)
-            for (int col = x; col < x + width; ++col)
-                pixelWriter.setColor(col, row, color);
-    }
 }
+
+/* Реализовать метод (класс) для заполнения эллипса. Добавить возможность
+интерполяции цвета, например, при удалении от центра фигуры. На вход могут
+поступать координаты левого верхнего угла вместе с шириной и высотой фигуры.
+10
+Или центр эллипса и размеры полуосей a и b. Возможны и другие варианты. [2],
+[3]
+*/
