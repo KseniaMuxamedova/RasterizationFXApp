01db4482ac1b53299a2b88270b3bf779e23b79f1
KseniaMyh <vy33frb8>
2022-11-11 15:25:46 +0300

    Версия 5: разобралась с цветами

--- src/main/java/com/cgvsu/rasterization/Rasterization.java
+++ src/main/java/com/cgvsu/rasterization/Rasterization.java
@@ -10,53 +10,58 @@ public class Rasterization {
             final GraphicsContext graphicsContext,
             final int x0, final int y0,
             final int a, final int b,
-            int startRED, int startGREEN, int startBLUE,
-            int endRED, int endGREEN, int endBLUE
-            //final Color startColor,
-            /*final Color tailColor*/) {
+            //int startRED, int startGREEN, int startBLUE,
+            //int endRED, int endGREEN, int endBLUE,
+            Color startColor,
+            final Color tailColor) {
         final PixelWriter pixelWriter = graphicsContext.getPixelWriter();
-        Color c = Color.rgb(startRED, startGREEN, startBLUE);
-        //int cRed = toRGB(startColor.getRed());
-        //int cGreen = toRGB(startColor.getGreen());
-        //int cBlue = toRGB(startColor.getBlue());
-        //int red = findRaznost(cRed, toRGB(tailColor.getRed()), b);
-        //int green = findRaznost(cGreen, toRGB(tailColor.getGreen()), b);
-        //int blue = findRaznost(cBlue, toRGB(tailColor.getBlue()), b);
-        int redK = findRaznost(startRED, endRED, b);
-        int rk = findK(startRED, endRED);
-        int greenK = findRaznost(startGREEN, endGREEN, b);
-        int gk = findK(startGREEN, endGREEN);
-        int blueK = findRaznost(startBLUE, endBLUE, b);
-        int bk = findK(startBLUE, endBLUE);
+        //Color c = Color.rgb(startRED, startGREEN, startBLUE);
+        int cRed = toRGB(startColor.getRed());
+        int cGreen = toRGB(startColor.getGreen());
+        int cBlue = toRGB(startColor.getBlue());
+        int red = findRaznost(cRed, toRGB(tailColor.getRed()), b);
+        int green = findRaznost(cGreen, toRGB(tailColor.getGreen()), b);
+        int blue = findRaznost(cBlue, toRGB(tailColor.getBlue()), b);
+        int rk = findK(cRed, toRGB(tailColor.getRed()));
+        int gk = findK(cGreen, toRGB(tailColor.getGreen()));
+        int bk = findK(cBlue, toRGB(tailColor.getBlue()));
+        //int redK = findRaznost(startRED, endRED, b);
+        //int rk = findK(startRED, endRED);
+        //int greenK = findRaznost(startGREEN, endGREEN, b);
+        //int gk = findK(startGREEN, endGREEN);
+        //int blueK = findRaznost(startBLUE, endBLUE, b);
+        //int bk = findK(startBLUE, endBLUE);
         int x1, x2, x;
-        int rd = 0, gd = 0, bd = 0;
-        for (int i = y0 - b; i <= y0 + b; i++, rd++, gd++, bd++) {
+        int z = 1;
+        for (int i = y0 - b; i <= y0 + b; i++, z++) {
             x = (int) Math.sqrt(a * a * (1 - (float) (i - y0) * (float) (i - y0) / (float) b / (float) b));
             x1 = x0 - x;
             x2 = x0 + x;
             for (int j = x1; j <= x2; j++) {
-                pixelWriter.setColor(j, i, c);
+                pixelWriter.setColor(j, i, startColor);
             }
-            startRED = newC(startRED, redK, rd, rk);
+            cRed = newC(z, cRed, red, rk);
+            cGreen = newC(z, cGreen, green, gk);
+            cBlue = newC(z, cBlue, blue, bk);
+            /*startRED = newC(cRED, redK, rd, rk);
             startGREEN = newC(startGREEN, greenK, gd, gk);
-            startBLUE = newC(startBLUE, blueK, bd, bk );
-            System.out.println(startBLUE);
-            c = Color.rgb(startRED, startGREEN, startBLUE);
-            rd =kofe(rd, redK);
+            startBLUE = newC(startBLUE, blueK, bd, bk );*/
+            startColor = Color.rgb(cRed, cGreen, cBlue);
+            /*rd =kofe(rd, redK);
             gd = kofe(gd, greenK);
-            bd = kofe(bd, blueK);
+            bd = kofe(bd, blueK);*/
         }
     }
 
     private static int toRGB(double c) {
-        return (int) (c * 255);
+        return (int) (c * 186);
     }
 
-    private static int newC(int c, int a, int d, int k){
-        if(d == a){
-            c += k;
+    private static int newC(int i, int st, int d, int k){
+        if(i % d == 0 && st + k > 0 && st + k < 255){
+            st += k;
         }
-        return c;
+        return st;
     }
 
     private static int kofe(int c, int a){
@@ -70,7 +75,7 @@ public class Rasterization {
         if(start == tail){
             return 2*b;
         }
-        return Math.round((float)(2 * b) / Math.abs(start - tail));
+        return Math.round(Math.abs((2 * b) / (start - tail)));
     }
 
     private static int findK(int start, int tail){
--- src/main/java/com/cgvsu/rasterizationfxapp/RasterizationController.java
+++ src/main/java/com/cgvsu/rasterizationfxapp/RasterizationController.java
@@ -21,7 +21,14 @@ public class RasterizationController {
         anchorPane.prefHeightProperty().addListener((ov, oldValue, newValue) -> canvas.setHeight(newValue.doubleValue()));
         Color c1 = Color.rgb(239, 9, 205);
         Color c2 = Color.rgb(64, 204, 1);
-        Rasterization.drawOval(canvas.getGraphicsContext2D(), 400, 300, 300, 200, 239, 9, 205,  64, 204, 1);
+        Rasterization.drawOval(canvas.getGraphicsContext2D(), 400, 300, 300, 200, c1, c2);
+        Color c3 = Color.rgb(0, 0, 0);
+        Color c4 = Color.rgb(255, 255, 255);
+        Rasterization.drawOval(canvas.getGraphicsContext2D(), 150, 220, 150, 220, c3, c4);
+        /*Color c5 = Color.rgb(255, 255, 255);
+        Color c6 = Color.rgb(0, 0, 0);
+        Rasterization.drawOval(canvas.getGraphicsContext2D(), 200, 350, 100, 120, c5, c6);*/
+
     }
 
 }
